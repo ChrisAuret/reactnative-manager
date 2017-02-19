1b85b4d11eeed14c2f657de4f544e39560ddfc5a
Chris Auret <chris.auret@yahoo.co.uk>
2017-02-19 20:50:11 +1100

    dynamic proprty updates

--- src/components/employeeCreate.js
+++ src/components/employeeCreate.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './Common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="jane"
+            value={this.props.name}
+            onCHangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+          />
+        </CardSection>
+
+
+        <CardSection>
+          <Input
+            label="phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+
+        </CardSection>
+      </Card>
+
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.EmployeeForm;
 
-export default EmployeeCreate;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
