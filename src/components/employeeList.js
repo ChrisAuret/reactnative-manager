ced1eef257d7730d71041e7c3a0adf4f28241558
Chris Auret <chris.auret@yahoo.co.uk>
2017-02-20 11:27:58 +1100

    dynamic datasource building

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
+import { connect } from 'react-redux';
+import {
+  employeesFetch
+} from '../actions';
 
 class EmployeeList extends Component {
+
+  componentWillMount() {
+    this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componenetWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that hte componentWillMount
+    // will be rendered with
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
     return (
       <View>
@@ -16,4 +41,4 @@ class EmployeeList extends Component {
 }
 
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
